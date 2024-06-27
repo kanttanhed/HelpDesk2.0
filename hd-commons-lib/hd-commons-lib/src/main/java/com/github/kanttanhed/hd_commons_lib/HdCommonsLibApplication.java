bda7883dad859fb49f9486e57b8bd330e8d65158
Plinio <plinio.salazar@yahoo.com.br>
2024-06-27 22:37:10 +0100

    chore: set up commons lib

---
+++ hd-commons-lib/hd-commons-lib/src/main/java/com/github/kanttanhed/hd_commons_lib/HdCommonsLibApplication.java
@@ -0,0 +1,13 @@
+package com.github.kanttanhed.hd_commons_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdCommonsLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdCommonsLibApplication.class, args);
+	}
+
+}
